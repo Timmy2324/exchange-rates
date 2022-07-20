ddaa932833e9796fdd910470bdefc1d34cb2885a
Timmy2324 <salikhov.t.s@yandex.ru>
2022-07-20 22:46:04 +0400

    added logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import s from "./Header.module.css";
 import Select from "../Select/Select";
+import logo from "../../img/logo.svg";
 
 const Header = () => {
     const arrLanguage = ["RU", "ENG"]
     return (
         <header className={s.header}>
             <div className={`${s.headerWrapper} container`}>
-                <span>LOGO</span>
+                <div className={s.logo}>
+                    <img alt={'logo'} className={s.logoImg} src={logo}/>
+                    <span className={s.logoName}>exchange rate</span>
+                </div>
+
                 <Select options={arrLanguage}/>
             </div>
         </header>
